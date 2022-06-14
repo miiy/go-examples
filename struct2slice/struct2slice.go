3b0718cfd4466e3ccdb6ac202573ae2e200a4060
miiy <miiy@users.noreply.github.com>
2022-06-14 09:04:39 +0800

    Update

--- struct2slice/struct2slice.go
+++ struct2slice/struct2slice.go
@@ -1,9 +1,22 @@
 package struct2slice
 
 import (
-	"fmt"
 	"reflect"
 )
+
+func StructToSlice(v interface{}) []interface{} {
+	to := reflect.TypeOf(v)
+	vo := reflect.ValueOf(v)
+	if to.Kind() == reflect.Ptr {
+		to = to.Elem()
+		vo = vo.Elem()
+	}
+	var s []interface{}
+	for i := 0; i < to.NumField(); i++ {
+		s = append(s, vo.Field(i).Interface())
+	}
+	return s
+}
 
 func StructToSliceByTag(v interface{}, t string) []interface{} {
 	to := reflect.TypeOf(v)
@@ -15,8 +28,22 @@ func StructToSliceByTag(v interface{}, t string) []interface{} {
 	var s []interface{}
 	for i := 0; i < to.NumField(); i++ {
 		if to.Field(i).Tag.Get(t) == "1" {
-			s = append(s, fmt.Sprintf("%v", vo.Field(i)))
+			s = append(s, vo.Field(i).Interface())
 		}
+	}
+	return s
+}
+
+func StructToSliceByFieldName(v interface{}, fields []string) []interface{} {
+	to := reflect.TypeOf(v)
+	vo := reflect.ValueOf(v)
+	if to.Kind() == reflect.Ptr {
+		to = to.Elem()
+		vo = vo.Elem()
+	}
+	var s []interface{}
+	for _, field := range fields {
+		s = append(s, vo.FieldByName(field).Interface())
 	}
 	return s
 }
--- struct2slice/struct2slice_test.go
+++ struct2slice/struct2slice_test.go
@@ -8,7 +8,7 @@ type TestStudent struct {
 	Class string `a:"1" b:"0"`
 }
 
-func TestStructToSlice(t *testing.T) {
+func TestStructToSliceByTag(t *testing.T) {
 	b := &TestStudent{
 		Name:  "zhangsan",
 		Age:   19,
@@ -18,4 +18,28 @@ func TestStructToSlice(t *testing.T) {
 	t.Log(s1)
 	s2 := StructToSliceByTag(b, "b")
 	t.Log(s2)
+}
+
+func TestStructToSlice(t *testing.T) {
+	b := &TestStudent{
+		Name:  "zhangsan",
+		Age:   19,
+		Class: "class1",
+	}
+	s1 := StructToSlice(b)
+	t.Log(s1)
+}
+
+func TestStructToSliceByFieldName(t *testing.T) {
+	b := &TestStudent{
+		Name:  "zhangsan",
+		Age:   19,
+		Class: "class1",
+	}
+	s1 := StructToSliceByFieldName(b, []string{"Name", "Age"})
+	t.Log(s1)
+	s2 := StructToSliceByFieldName(b, []string{"Name", "Age", "Class"})
+	t.Log(s2)
+	//s3 := StructToSliceByFieldName(b, []string{"Name", "Age", "Score"})
+	//t.Log(s3)
 }
